b62a0590a5403c268e1ab204f23e7b1f2fc93858
lemunozm <lemunozm@gmail.com>
2022-05-23 21:39:16 +0200

    Added .p10k.zsh config. Fixed links script

--- make_symbolic_links.sh
+++ make_symbolic_links.sh
@@ -1,12 +1,13 @@
-ln -is config.toml ~/.cargo/config.toml
-ln -is .tmux.conf ~/.tmux.conf
-ln -is .gitconfig ~/.gitconfig
-ln -is .zshrc ~/.zshrc
+ln -is $PWD/config.toml $HOME/.cargo/config.toml
+ln -is $PWD/.tmux.conf $HOME/.tmux.conf
+ln -is $PWD/.gitconfig $HOME/.gitconfig
+ln -is $PWD/.zshrc $HOME/.zshrc
+ln -is $PWD/.p10k.zsh $HOME/.p10k.zsh
 
 if [ ! -L "$HOME/.config/alacritty" ] ; then
-    ln -s alacritty ~/.config/alacritty
+    ln -s $PWD/alacritty $HOME/.config/alacritty
 fi
 
 if [ ! -L "$HOME/.config/nvim" ] ; then
-    ln -s nvim ~/.config/nvim
+    ln -s $PWD/nvim $HOME/.config/nvim
 fi
