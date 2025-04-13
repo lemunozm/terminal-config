a8da2af888b0655cf30a2238319923e5b6366279
lemunozm <lemunozm@gmail.com>
2025-04-14 01:37:51 +0200

    add flutter config

--- nvim/init.lua
+++ nvim/init.lua
@@ -346,6 +346,70 @@ require("lazy").setup(
       "aklt/plantuml-syntax",
       event = { 'BufReadPre *.puml', 'BufNewFile *.puml' },
     },
+    {
+      'nvim-flutter/flutter-tools.nvim',
+      lazy = false,
+      dependencies = {
+          'nvim-lua/plenary.nvim',
+          'stevearc/dressing.nvim', -- optional for vim.ui.select
+          'hrsh7th/nvim-cmp',
+      },
+      config = function()
+        local function on_attach(_, bufnr)
+          local function cmd(mode, lhs, rhs)
+            vim.keymap.set(mode, lhs, rhs, { noremap = true, buffer = true })
+          end
+
+          vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+          cmd('n', 'gd', vim.lsp.buf.definition)
+          cmd('n', 'K', vim.lsp.buf.hover)
+
+          -- <leader>n will jump to the next Lean line with a diagnostic message on it
+          -- <leader>N will jump backwards
+          cmd('n', '<leader>n', function() vim.diagnostic.goto_next{popup_opts = {show_header = false}} end)
+          cmd('n', '<leader>N', function() vim.diagnostic.goto_prev{popup_opts = {show_header = false}} end)
+
+          -- <leader>K will show all diagnostics for the current line in a popup window
+          cmd('n', '<leader>K', function() vim.diagnostic.open_float(0, { scope = "line", header = false, focus = false }) end)
+
+          -- <leader>q will load all errors in the current lean file into the location list
+          -- (and then will open the location list)
+          -- see :h location-list if you don't generally use it in other vim contexts
+          cmd('n', '<leader>q', vim.diagnostic.setloclist)
+
+          --Show the message with <space>e
+          cmd('n', '<leader>e', vim.diagnostic.open_float)
+
+          -- disable virtual_text (inline) diagnostics and use floating window
+          -- format the message such that it shows source, message and
+          -- the error code.
+          vim.diagnostic.config({
+            virtual_text = false,
+            signs = true,
+            float = {
+              border = "single",
+              format = function(diagnostic)
+                return string.format(
+                  "%s (%s) [%s]",
+                  diagnostic.message,
+                  diagnostic.source,
+                  diagnostic.code or diagnostic.user_data.lsp.code
+                )
+              end,
+            },
+          })
+        end
+
+        require('flutter-tools').setup({
+          widget_guides = { enabled = true },
+          lsp = { on_attach = on_attach },
+          dev_log = {
+            open_cmd = "15split",
+          },
+        })
+      end
+    },
 
     -- Themes
     { "catppuccin/nvim",
