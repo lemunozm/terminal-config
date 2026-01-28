6b79b5d08da43a8503862dcd10967a97e76fc58a
lemunozm <lemunozm@gmail.com>
2026-01-28 08:37:29 +0100

    add oil.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -89,6 +89,31 @@ require("lazy").setup(
     },
     { "guns/xterm-color-table.vim" },
     { "airblade/vim-rooter" },
+    { "stevearc/oil.nvim",
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      keys = {
+        { "-", "<cmd>Oil<cr>" },
+      },
+      config = function()
+        require("oil").setup({
+          keymaps = {
+            ["<C-h>"] = false,
+            ["<C-l>"] = false,
+            ["<C-p>"] = { "actions.preview", opts = { vertical = false, split = "belowright" } },
+            ["<C-f>"] = "actions.refresh",
+          },
+        })
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "OilEnter",
+          callback = vim.schedule_wrap(function(args)
+            local oil = require("oil")
+            if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+              oil.open_preview({ vertical = false, split = "belowright" })
+            end
+          end),
+        })
+      end
+    },
     { "zefei/vim-wintabs",
       lazy = false,
       keys = {
