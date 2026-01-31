e7c89c00be89e23e8814aba481d710d58edfecd6
lemunozm <lemunozm@gmail.com>
2026-01-31 16:53:30 +0100

    Start in the bottom after opening a dump file

--- nvim/init.lua
+++ nvim/init.lua
@@ -469,6 +469,7 @@ require("lazy").setup(
             vim.opt_local.signcolumn = "no"
             vim.keymap.set("n", "<esc>", "<cmd>q!<cr>", { buffer = true })
             vim.keymap.set("n", "q", "<cmd>q!<cr>", { buffer = true })
+            vim.cmd("normal! G")
           end,
         })
       end,
