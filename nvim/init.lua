50aa069ccb1ea7f8ece91b26e0430228cfa435c4
lemunozm <lemunozm@gmail.com>
2026-02-01 16:33:25 +0100

    add no-deck-pain neovim plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -116,6 +116,32 @@ require("lazy").setup(
         },
       },
     },
+    { "shortcuts/no-neck-pain.nvim",
+      version = "*",
+      keys = {
+        { "<leader>c", "<cmd>NoNeckPain<cr>" },
+      },
+      opts = {
+        width = 100,
+        buffers = {
+          right = { enabled = false },
+        },
+      },
+      config = function(_, opts)
+        require("no-neck-pain").setup(opts)
+        local widths = {
+          solidity = 120,
+        }
+        vim.api.nvim_create_autocmd("BufEnter", {
+          callback = function()
+            local state = require("no-neck-pain.state")
+            if not state.enabled then return end
+            local w = widths[vim.bo.filetype] or opts.width
+            vim.cmd("NoNeckPainResize " .. w)
+          end,
+        })
+      end,
+    },
     { "akinsho/git-conflict.nvim",
       version = "*",
       opts = {
